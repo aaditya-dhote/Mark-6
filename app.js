bcf86fdc7adccdab74159f49bbd9151f5b8ad5bc
Aaditya Dhote <AadityaDhote@outlook.com>
2022-09-03 23:23:37 +0530

    completed ex04

--- app.js
+++ app.js
@@ -1,4 +1,44 @@
 // var prompt = prompt("give me your name")
 // alert("Welcome to Banana speaks"+prompt)
 var btnTranslate=document.querySelector("#btn-translate");
-var btnTranslate=addEventListener("click",function clickEventHandler(){console.log("clicked")});
+
+// var btnTranslate=addEventListener("click",function clickEventHandler(){console.log("clicked")});
+
+// textarea box
+var txtInput = document.querySelector("#txt-input");
+console.log(txtInput)
+function clickHandler(){
+    console.log("clicked");
+    console.log("input",txtInput.value);
+};
+btnTranslate.addEventListener("click",clickHandler)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var txtInput = document.querySelector("#txt-input");
+// console.log(txtInput)
+// function clickHandler(){console.log("clicked!");
+// console.log("input",txtInput.value);
+
+// }
