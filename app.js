159209f3d46508de62b7abeeebf88da0e1db5a20
Aaditya Dhote <AadityaDhote@outlook.com>
2022-09-03 15:09:55 +0530

    added callback

---
+++ app.js
@@ -0,0 +1,4 @@
+// var prompt = prompt("give me your name")
+// alert("Welcome to Banana speaks"+prompt)
+var btnTranslate=document.querySelector("#btn-translate");
+var btnTranslate=addEventListener("click",function clickEventHandler(){console.log("clicked")});
