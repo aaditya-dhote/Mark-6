67cac5f96d2eaeaa0fb473f4e892f3d6e6b23ea5
Aaditya Dhote <AadityaDhote@outlook.com>
2022-09-15 11:41:12 +0530

    added

--- app.js
+++ app.js
@@ -1,44 +1,23 @@
-// var prompt = prompt("give me your name")
-// alert("Welcome to Banana speaks"+prompt)
-var btnTranslate=document.querySelector("#btn-translate");
-
-// var btnTranslate=addEventListener("click",function clickEventHandler(){console.log("clicked")});
-
-// textarea box
 var txtInput = document.querySelector("#txt-input");
-console.log(txtInput)
+var btnTranslate=document.querySelector("#btn-translate");
+var outputDiv= document.querySelector("#output");
+
+
+var serverURL ="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input){
+    return serverURL + "?" +"text=" + input}
+
+
 function clickHandler(){
-    console.log("clicked");
-    console.log("input",txtInput.value);
+var inputText = txtInput.value;
+
+fetch(getTranslationURL(inputText))
+.then (response=>response.json())
+.then(json=>{
+    var translatedText = json.contents.translated;
+    outputDiv.innerText = translatedText;
+})
+
 };
 btnTranslate.addEventListener("click",clickHandler)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var txtInput = document.querySelector("#txt-input");
-// console.log(txtInput)
-// function clickHandler(){console.log("clicked!");
-// console.log("input",txtInput.value);
-
-// }
